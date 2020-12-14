f4ac0f19e617881cef9a21774ff8d8bcf07fc5f4
Aleks_Meleshko <aleks_meleshko.mail.ru>
2020-12-15 00:06:02 +0300

    modified tasks 2_1, 2_2

--- lesson_2/task_2_2.py
+++ lesson_2/task_2_2.py
@@ -1,11 +1,10 @@
 num_of_items = int(input("Enter the number of list items: "))
 my_list = []
-i = 0
-while i < num_of_items:
+item = 0
+while item < num_of_items:
     my_list.append(input("Enter the following list value: "))
-    i += 1
-el = 0
-for elem in range(int(len(my_list) / 2)):
-    my_list[el], my_list[el + 1] = my_list[el + 1], my_list[el]
-    el += 2
-print(my_list)
+    item += 1
+print(f"Current list: {my_list}")
+for i in range(1, num_of_items, 2):
+    my_list[i - 1], my_list[i] = my_list[i], my_list[i-1]
+print(f"Result: {my_list}")

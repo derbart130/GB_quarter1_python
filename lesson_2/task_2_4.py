7b108cbd3a7f51287ba4a33ad25ab6589b7f53d3
Aleks_Meleshko <aleks_meleshko.mail.ru>
2020-12-15 00:19:27 +0300

    modified task 2_4

--- lesson_2/task_2_4.py
+++ lesson_2/task_2_4.py
@@ -1,7 +1,4 @@
 string = input("Enter the string: ")
 words = string.split()
-for ind, el in enumerate(words, 1):
-    if len(el) <= 10:
-        print(ind, el)
-    else:
-        print(ind, el[:10])
+for ind, word in enumerate(words, 1):
+    print(f"{ind}. {word[:10]}")

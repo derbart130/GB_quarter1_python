0cdeffec1f71271f27e4feeba9339970e5e3c925
Aleks_Meleshko <aleks_meleshko.mail.ru>
2020-12-20 00:07:14 +0300

    modified tasks 4_1, 4_6_b, 4_7

--- lesson_4/task_4_1.py
+++ lesson_4/task_4_1.py
@@ -1,18 +1,7 @@
-#from sys import argv
-
-#file_name, worked_hour, rate, benefit = argv
-
-#calculation = (int(worked_hour) * int(rate)) + int(benefit)
-#print(f"Your pay is equal {calculation}")
-
 from sys import argv
 
-name, time, salary, bonus = argv
-try:
-    time = int(time)
-    salary = int(salary)
-    bonus = int(bonus)
-    res = time * salary + bonus
-    print(f'заработная плата сотрудника  {res}')
-except ValueError:
-    print('Not a number')
+file_name, worked_hour, rate, benefit = argv
+
+result = (int(worked_hour) * int(rate)) + int(benefit)
+
+print(f"Your estimated salary {result}")
--- lesson_4/task_4_6_b.py
+++ lesson_4/task_4_6_b.py
@@ -1,1 +1,21 @@
-св
+from itertools import cycle
+
+
+def fill_my_list(el):
+    my_list = []
+    for i in range(el):
+        my_list.append(input("Enter list item: "))
+    return my_list
+
+
+def my_func(list_, iteration):
+    i = 0
+    iter = cycle(list_)
+    while i < iteration:
+        print(next(iter))
+        i += 1
+
+
+el = int(input("Enter number of list elements: "))
+
+my_func(fill_my_list(el), iteration=int(input("Enter number of iterations: ")))

eb2c43ce307bbf4aadbdd42f40780a8306bc58c8
Aleks_Meleshko <aleks_meleshko.mail.ru>
2020-12-22 21:03:01 +0300

    modified task_4_5

--- lesson_4/task_4_5.py
+++ lesson_4/task_4_5.py
@@ -5,8 +5,6 @@ def my_func(a_1, a_2):
     return a_1 * a_2
 
 
-my_list = []
-for el in range(100, 1001, 2):
-    my_list.append(el)
+my_list = [el for el in range(100, 1001, 2)]
 
 print(reduce(my_func, my_list))

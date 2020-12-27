8e514092c0dcf88361c208333770b8d45cd2dc3f
Aleks_Meleshko <aleks_meleshko.mail.ru>
2020-12-27 07:22:56 +0300

    modified some files

--- lesson_5/task_5_2.py
+++ lesson_5/task_5_2.py
@@ -1,11 +1,3 @@
-my_list = ['Nemo enim ipsam voluptatem\n',
-           'Itaque earum rerum hic tenetur a sapiente delectus\n',
-           'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet\n',
-           'Sed ut perspiciatis, qui in ea voluptate velit esse\n',
-           'Sed ut perspiciatis\n'
-           ]
-with open("test_2.txt", 'w+', encoding="utf-8") as file_obj:
-    file_obj.writelines(my_list)
 with open("test_2.txt") as file_obj:
     my_line = file_obj.readlines()
     for line, words in enumerate(my_line, 1):
--- lesson_5/task_5_3.py
+++ lesson_5/task_5_3.py
@@ -1,12 +1,3 @@
-my_list = {'John Smith': 21300,
-           'Ivan Drago': 17600,
-           'Rokky Balboa': 64700,
-           'John Nollan': 35400,
-           'Gregory House': 13550
-           }
-file_obj = open("test_3.txt", 'w', encoding='utf-8')
-for name, salary in my_list.items():
-    file_obj.write(name + ':' + str(salary) + "\n")
 with open("test_3.txt", "r") as file_obj:
     my_dict = {line.split(':')[0]: float(line.split(':')[1]) for line in file_obj}
     print(f"Low salary (<20000): {[el[0] for el in my_dict.items() if el[1] < 20000]}")
